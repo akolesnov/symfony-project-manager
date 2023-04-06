83bbc87306ef119ff97065b79c884b86ef1b28b5
akolesnov <a.kolesnov@yandex.ru>
2023-04-07 00:18:20 +0300

    Extracted user builder

--- manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -5,18 +5,16 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
 
 use App\Model\User\Entity\User\Email;
-use App\Model\User\Entity\User\Id;
-use App\Model\User\Entity\User\User;
+use App\Tests\Builder\User\UserBuilder;
 use PHPUnit\Framework\TestCase;
-use Ramsey\Uuid\Uuid;
 
 class RequestTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $user = new User(
-            $id = ID::next(),
-            $date = new \DateTimeImmutable(),
+        $user = (new UserBuilder())->build();
+
+        $user->signUpByEmail(
             $email = new Email('test@app.test'),
             $hash = 'hash',
             $token = 'token'
@@ -25,10 +23,23 @@ class RequestTest extends TestCase
         self::assertTrue($user->isWait());
         self::assertFalse($user->isActive());
 
-        self::assertEquals($id, $user->getId());
-        self::assertEquals($date, $user->getDate());
         self::assertEquals($email, $user->getEmail());
         self::assertEquals($hash, $user->getPasswordHash());
         self::assertEquals($token, $user->getConfirmToken());
+    }
+
+    public function testAlready(): void
+    {
+        $user = (new UserBuilder())->build();
+
+        $user->signUpByEmail(
+            $email = new Email('test@app.test'),
+            $hash = 'hash',
+            $token = 'token'
+        );
+
+        $this->expectExceptionMessage('User is already signed up.');
+
+        $user->signUpByEmail($email, $hash, $token);
     }
 }
