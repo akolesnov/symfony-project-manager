d17ad45bde17dfba0efa9b85793889c53e8b69ab
akolesnov <a.kolesnov@yandex.ru>
2023-04-13 23:32:50 +0300

    Implemented confirm email sending

--- manager/src/Model/User/Service/ConfirmTokenSender.php
+++ manager/src/Model/User/Service/ConfirmTokenSender.php
@@ -3,11 +3,31 @@
 namespace App\Model\User\Service;
 
 use App\Model\User\Entity\User\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ConfirmTokenSender
 {
+    private $mailer;
+    private $from;
+
+    public function __construct(MailerInterface $mailer, string $from)
+    {
+        $this->mailer = $mailer;
+        $this->from = $from;
+    }
+
     public function send(Email $email, string $token): void
     {
-        
+        $message = (new TemplatedEmail())
+            ->from($this->from)
+            ->to($email->getValue())
+            ->subject('Sign Up Confirmation.')
+            ->htmlTemplate('mail/user/signup.html.twig')
+            ->context(['token' => $token]);
+            
+        if (!$this->mailer->send($message)) {
+            throw new \RuntimeException('Unable to send message.');
+        }
     }
 }
