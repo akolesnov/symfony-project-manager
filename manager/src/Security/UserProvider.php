a013e77760902e08c622d1823afad6f876f1f704
akolesnov <a.kolesnov@yandex.ru>
2023-04-15 16:04:36 +0300

    Added runtime identity checking

--- manager/src/Security/UserProvider.php
+++ manager/src/Security/UserProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Security;
 
+use App\ReadModel\User\AuthView;
 use App\ReadModel\User\UserFetcher;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
@@ -19,21 +20,10 @@ class UserProvider implements UserProviderInterface
         $this->users = $users;
     }
 
-    public function loadUserByIdentifier(string $identifier): UserInterface
+    public function loadUserByIdentifier(string $username): UserInterface
     {
-        $user = $this->users->findForAuth($identifier);
-
-        if (!$user) {
-            throw new UserNotFoundException('');
-        }
-
-        return new UserIdentity(
-            $user->id,
-            $user->email,
-            $user->password_hash,
-            $user->role,
-            $user->status
-        );
+        $user = $this->loadUser($username);
+        return self::identityByUser($user);
     }
 
     public function supportsClass(string $class): bool
@@ -46,7 +36,26 @@ class UserProvider implements UserProviderInterface
         if (!$user instanceof UserIdentity) {
             throw new UnsupportedUserException('Invalid user class ' . \get_class($user));
         }
+        $user = $this->loadUser($user->getUsername());
+        return self::identityByUser($user);
+    }
 
+    private function loadUser($username): AuthView
+    {
+        if (!$user = $this->users->findForAuth($username)) {
+            throw new UserNotFoundException('');
+        }
         return $user;
+    }
+
+    private static function identityByUser(AuthView $user): UserIdentity
+    {
+        return new UserIdentity(
+            $user->id,
+            $user->email,
+            $user->password_hash,
+            $user->role,
+            $user->status
+        );
     }
 }
